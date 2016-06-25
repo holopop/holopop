6ff9c9a6e24d55abf72d9e05692f69af925a608b
Lucas Caro <lucascaro@gmail.com>
2016-06-25 12:20:24 -0700

    Add audop to belly button

---
+++ Assets/Scripts/HoloPop/PlayAudoOnTap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudoOnTap : MonoBehaviour {
+
+    public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        this.audio = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnPlayTap()
+    {
+        if (this.audio.isPlaying)
+        {
+            this.audio.Stop();
+        }
+        else
+        {
+            this.audio.Play();
+        }
+    }
+}
